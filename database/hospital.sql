43c5297e48e0aa857e140661dd56f568804d8465
ShirlaSantana <shirlalml@gmail.com>
2024-04-12 08:40:00 -0600

    save data sql file

--- database/hospital.sql
+++ database/hospital.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO hospitalDB.Hospital (id,name,city) VALUES
+INSERT INTO hospitalDB.Hospital (id,name,zipCode) VALUES
 	 (1,'IMSS La Ceiba','97845'),
 	 (2,'Hospital El Faro','97231'),
 	 (3,'Hospital militar','97456'),
